c0273d1698b96b891f53e1ba505e374ce7b9d05a
OUSSAMA <oussamacharef71@gmail.com>
2026-04-10 23:23:03 +0100

    complete article management module and UI updates

--- blog_manager/index.php
+++ blog_manager/index.php
@@ -2,21 +2,16 @@
 
 
 require 'connexion.php';
-
 require 'article.php';
 
 
 $database = new Database();
-
 $db = $database->getConnection();
 
 $article_obj = new Article($db);
-
-
 $articles = $article_obj->all();
 
 ?>
-
 
 
 
@@ -24,59 +19,54 @@ $articles = $article_obj->all();
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Management Blog</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Post Manager</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
 
-    <div class="header-container">
+    <div class="header_container">
         <h1>Post Manager</h1>
-
-
-        <a href="create.php" class="btn-add"> + New Article</a>
-
+        <a href="create.php" class="btn_add">+ New Article</a>
     </div>
+    <div class="posts_grid">
+    <?php if(!empty($articles)): ?>
+        <?php foreach($articles as $article): ?>
+            <div class="post_card">
+                    <?php if (!empty($article['image_url'])): ?>
+                        <img class="post_card_img" src="<?php echo $article['image_url']; ?>">
+                    <?php else: ?>
+                        
+                          <p class="image_not" >Image not available</p>  
+                        
+                    <?php endif; ?>                
+                    <div class="post_card_body">
+                        <span class="post_card_category">
+                            <?php echo htmlspecialchars($article['category_name'] ?? 'General'); ?>
+                        </span>
+        
+                         <h2 class="post_card_title"><?php echo htmlspecialchars($article['title']); ?></h2>
+        
+                         <p class="post_card_content">
+                                <?php echo htmlspecialchars($article['content']); ?>
+                        </p>
+                        <div class="post_card_footer">
+                            <span>By <strong><?php echo htmlspecialchars($article['user_name']); ?></strong></span>
+                            <span><?php echo date('M d, Y', strtotime($article['publish_date'])); ?></span>
+                        </div>
+                    </div>
 
-
-    <div class="table-wrapper">
-        <table>
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Article Info</th>
-                    <th>Author</th>
-                    <th>Category</th>
-                    <th>Date</th>
-                    <th>Status</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach($articles as $article): ?>
-
-
-                    <tr>
-                        <td style="color: #999;"><?php echo $article['id_post']; ?></td>
-                        <td>
-                            <span class="post-title"><?php echo htmlspecialchars($article['title']); ?></span>
-                            <div class="post-content"><?php echo nl2br(htmlspecialchars($article['content'])); ?></div>
-                        </td>
-                        <td class="author-name"><?php echo htmlspecialchars($article['user_name']); ?></td>
-                        <td><span class="category-badge"><?php echo htmlspecialchars($article['category_name']) ?? 'General'; ?></span></td>
-                        <td style="white-space: nowrap; font-size: 13px;"><?php echo date('M d, Y', strtotime($article['publish_date'])); ?></td>
-                        <td>
-                            <span class="status-pill <?php  echo $article['status'];?>">
-                                <?php echo $article['status']; ?>
-                            </span>
-                        </td>
-                    </tr>
-
-                    <?php endforeach; ?>
-            </tbody>
-        </table>
-
-    </div>
-
- 
+                    <div class="post_actions">
+                        <a href="edit.php?id=<?php echo $article['id_post']; ?>" class="btn_edit">Edit</a>
+                        <a href="delete.php?id=<?= $article['id_post'] ?>" class="btn_delete" onclick="return confirm('Are you sure?')">Delete</a>
+                    </div>
+            </div>
+        <?php endforeach; ?>
+    <?php else: ?>
+        <p>No posts found.</p>
+    <?php endif; ?>
+</div>
+    
 </body>
 </html>
