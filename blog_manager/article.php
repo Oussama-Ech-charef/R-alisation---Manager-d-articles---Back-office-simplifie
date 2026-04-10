36cc9eb4d1bcaf0ce78c9df4a28f7c40d8a9ed94
OUSSAMA <oussamacharef71@gmail.com>
2026-04-10 23:19:39 +0100

    refactor(article): enhance code structure and logic after initial push

--- blog_manager/article.php
+++ blog_manager/article.php
@@ -1,6 +1,8 @@
 <?php
 
+
 class Article {
+
     private $conn;
 
 
@@ -14,7 +16,7 @@ class Article {
     }
 
 
-    public function all() {
+     public function all() {
         $query = "SELECT p.*, u.user_name, c.category_name 
                     from {$this->table_name} p
                     join users u on p.id_user = u.id_user 
@@ -22,20 +24,15 @@ class Article {
                     order by p.publish_date desc";
 
 
-        $stmt = $this->conn->prepare($query);
-
-        $stmt->execute();
-
+        $stmt = $this->conn->query($query);
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
-
-
-    public function create($title, $content, $id_user, $id_category,$status) {
+    public function create($title, $content, $id_user, $id_category,$status, $image_url) {
 
         $query = "INSERT INTO {$this->table_name}
-                     (title, content, id_user, id_category, status)
-                     VALUES (:title, :content, :id_user, :id_category, :status)";
+                     (title, content, id_user, id_category, status, image_url)
+                     VALUES (:title, :content, :id_user, :id_category, :status, :image_url)";
 
 
         $stmt = $this->conn->prepare($query);
@@ -46,6 +43,7 @@ class Article {
             $stmt->bindParam(':id_user', $id_user);
             $stmt->bindParam(':id_category', $id_category);
             $stmt->bindParam(':status', $status);
+            $stmt->bindParam(':image_url', $image_url);
 
 
         if($stmt->execute()) {
@@ -53,4 +51,42 @@ class Article {
         }
         return false;
     }
+
+    public function delete($id) {
+        $query = "DELETE FROM " . $this->table_name . " WHERE id_post = :id";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':id', $id);
+        return $stmt->execute();
+}
+
+    public function single($id) {
+        $query = "SELECT * FROM " . $this->table_name . " WHERE id_post = :id";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function update($id, $title, $content, $id_category, $status, $image_url) {
+    $query = "UPDATE " . $this->table_name . " 
+              SET title = :title,
+               content = :content,
+               id_category = :id_category,
+                status = :status, 
+                image_url = :image_url 
+              WHERE id_post = :id";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':id', $id);
+        $stmt->bindParam(':title', $title);
+        $stmt->bindParam(':content', $content);
+        $stmt->bindParam(':id_category', $id_category);
+        $stmt->bindParam(':status', $status);
+        $stmt->bindParam(':image_url', $image_url);
+        return $stmt->execute();
+}
+
+
+
+
+
 }
